7e8c9b56560e19c489b092892a0c2fa5c0180e33
Tom <tolas22@gmail.com>
2017-12-23 11:42:22 +0200

    start category

---
+++ KTRestaurant/App_Code/Category.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Category
+/// </summary>
+public class Category
+{
+    int categoryID;
+    string categoryName;
+    public Category()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public int CategoryID
+    {
+        get
+        {
+            return categoryID;
+        }
+
+        set
+        {
+            categoryID = value;
+        }
+    }
+
+    public string CategoryName
+    {
+        get
+        {
+            return categoryName;
+        }
+
+        set
+        {
+            categoryName = value;
+        }
+    }
+}
--- KTRestaurant/addCategory.aspx
+++ KTRestaurant/addCategory.aspx
@@ -1,16 +1,10 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="addCategory.aspx.cs" Inherits="addCategory" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPageAdmin.master" CodeFile="addCategory.aspx.cs" Inherits="addCategory" %>
 
-<!DOCTYPE html>
+ <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-    <div>
-    
-    </div>
-    </form>
-</body>
-</html>
+ </asp:Content>
+
+
+ <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderAdmin" Runat="Server">
+     
+     </asp:Content>
