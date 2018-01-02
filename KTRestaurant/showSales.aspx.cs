0ca0baebc6ac7eed12f381cb33e54a3c90518b0f
Tom <tolas22@gmail.com>
2018-01-02 23:09:01 +0200

    showSales Complete!

--- KTRestaurant/showSales.aspx.cs
+++ KTRestaurant/showSales.aspx.cs
@@ -10,82 +10,108 @@ public partial class showSales : System.Web.UI.Page
 {
     DBservices dbs = new DBservices();
     string li;
-    protected void RefreshPage(object o, EventArgs e)
-    {
-        Session["dd1"] = fcategoryDDL.SelectedValue;
-        fcategoryDDL.SelectedIndex = fcategoryDDL.Items.IndexOf(fcategoryDDL.Items.FindByValue(Convert.ToString(Session["dd1"])));
-    }
+    //protected void RefreshPage(object o, EventArgs e)
+    //{
+    //    Session["dd1"] = fcategoryDDL.SelectedValue;
+    //    fcategoryDDL.SelectedIndex = fcategoryDDL.Items.IndexOf(fcategoryDDL.Items.FindByValue(Convert.ToString(Session["dd1"])));
+    //}
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["adminLogin"] == null)
         {
             Response.Redirect("Login.aspx");
         }
-        if (!Page.IsPostBack)
-        {
+        //if (!Page.IsPostBack)
+        //{
 
-        fcategoryDDL.DataBind();
-        fcategoryDDL.Items.Insert(0, new ListItem("Choose Category Name", "0"));
-        }
+        //fcategoryDDL.DataBind();
+        //fcategoryDDL.Items.Insert(0, new ListItem("Choose Category Name", "0"));
+        //}
         
     }
 
-    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
-    {
+    //protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    //{
 
+    //    if (e.Row.RowType == DataControlRowType.DataRow)
+    //    {
+
+    //        int prodIndex = GetColumnIndexByName(GridView1, "product_id");
+    //        int prodNIndex = GetColumnIndexByName(GridView1, "Product Name");
+    //    //    string check = e.Row.Cells[catidIndex].Text;
+    //        int prod_id = Convert.ToInt32(e.Row.Cells[prodIndex].Text);
+    //        string[] prodCat = getProductName(prod_id);
+    //        e.Row.Cells[prodNIndex].Text = prodCat[0].ToString();
+    //        if (IsPostBack)
+    //        {
+    //        if ( fcategoryDDL.SelectedValue != prodCat[1])
+    //        {
+    //            e.Row.Visible = false;
+    //        }
+
+    //        }
+
+    //    }
+    //}
+    //public static int GetColumnIndexByName(GridView grid, string name)
+    //{
+    //    foreach (DataControlField col in grid.Columns)
+    //    {
+    //        if (col.HeaderText.ToLower().Trim() == name.ToLower().Trim())
+    //        {
+    //            return grid.Columns.IndexOf(col);
+    //        }
+    //    }
+
+    //    return -1; // in case there in no such field
+    //}
+    //public string[] getProductName(int id)
+    //{
+    //    string[] prodcat = new string[2];
+    //    dbs.Name = "*";
+    //    dbs.Table = "productN";
+    //    //Populating a DataTable from database.
+    //    DataTable dt = dbs.readproductNDataBase();
+
+
+    //    foreach (DataRow row in dt.Rows)
+    //    {
+    //        if (id == (Convert.ToInt32(row["product_id"])))
+    //        {
+    //            prodcat[0] = row["title"].ToString();
+    //            prodcat[1] = row["category_id"].ToString();
+    //            return prodcat;
+    //        }
+    //    }
+    //    return null;
+    //}
+
+    //protected void fcategoryDDL_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+
+    //    Session["selected"]= fcategoryDDL.SelectedItem;
+
+    //}
+
+
+
+    protected void GridView1_DataBound(object sender, GridViewRowEventArgs e)
+    {
         if (e.Row.RowType == DataControlRowType.DataRow)
         {
 
-            int prodIndex = GetColumnIndexByName(GridView1, "product_id");
-            int prodNIndex = GetColumnIndexByName(GridView1, "Product Name");
-        //    string check = e.Row.Cells[catidIndex].Text;
-            int prod_id = Convert.ToInt32(e.Row.Cells[prodIndex].Text);
-            string[] prodCat = getProductName(prod_id);
-            e.Row.Cells[prodNIndex].Text = prodCat[0].ToString();
-            if (fcategoryDDL.SelectedValue != "0" && fcategoryDDL.SelectedValue != prodCat[1])
+            DataRow dr = ((DataRowView)e.Row.DataItem).Row;
+            if (Convert.ToBoolean(dr["p_method"]))
             {
-                e.Row.Visible = false;
+            e.Row.Cells[4].Text = "Credit Card";
+
             }
-            
-        }
-    }
-    public static int GetColumnIndexByName(GridView grid, string name)
-    {
-        foreach (DataControlField col in grid.Columns)
-        {
-            if (col.HeaderText.ToLower().Trim() == name.ToLower().Trim())
+            else
             {
-                return grid.Columns.IndexOf(col);
+                e.Row.Cells[4].Text = "Cash";
             }
+        
+
         }
-
-        return -1; // in case there in no such field
-    }
-    public string[] getProductName(int id)
-    {
-        string[] prodcat = new string[2];
-        dbs.Name = "*";
-        dbs.Table = "productN";
-        //Populating a DataTable from database.
-        DataTable dt = dbs.readproductNDataBase();
-
-
-        foreach (DataRow row in dt.Rows)
-        {
-            if (id == (Convert.ToInt32(row["product_id"])))
-            {
-                prodcat[0] = row["title"].ToString();
-                prodcat[1] = row["category_id"].ToString();
-                return prodcat;
-            }
-        }
-        return null;
-    }
-
-    protected void fcategoryDDL_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
-        Session["selected"]= fcategoryDDL.SelectedItem;
-
     }
 }
